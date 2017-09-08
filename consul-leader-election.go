4de5bad8ed39573fdeb76940058b30c12b047171
Max Wilhelm <max.wilhelm@wywy.com>
2017-09-08 13:15:14 +0200

    fix: try to acquire key if key has no session

--- consul-leader-election.go
+++ consul-leader-election.go
@@ -52,7 +52,7 @@ func main() {
 		os.Exit(errorExitCode)
 	}
 
-	if kv != nil {
+	if kv.Session != "" {
 		sessionInfo, _, err := client.Session().Info(kv.Session, &consul.QueryOptions{})
 		if err != nil {
 			fmt.Println(err.Error())
